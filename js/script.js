b823ba35e05a723be821c35e646fdcd837a50d8f
Nicolaj Thygesen <nico936d@edu.campusvejle.dk>
2020-11-30 12:58:45 +0100

    Added figures  to index.html through script.js and made some style in style.css with the featured class

--- js/script.js
+++ js/script.js
@@ -4,4 +4,22 @@
 
 // Nicolaj js
 
+const articleFunction = () =>{
+    const imagesLink = [
+        "img/find_butik.png",
+        "img/facebook.png",
+        "img/club_bs.png"
+    ]
+    const section = document.querySelector(".featured");
+    imagesLink.forEach((img) =>{
+        const figure = document.createElement("figure");
+        figure.classList.add("imageFigure");
+        const image = document.createElement("img");
+        image.src = img;
+        console.log(img);
+        figure.appendChild(image);
+        section.appendChild(figure);
+    })
+}
+articleFunction();
 // Nicolaj slut her
